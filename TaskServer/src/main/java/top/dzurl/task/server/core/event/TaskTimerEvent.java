70f25424a63e968e7f7eb87800a07f9d7b5f3fb7
lianshufeng <251708339@qq.com>
2021-03-09 13:01:50 +0800

    update

--- TaskServer/src/main/java/top/dzurl/task/server/core/event/TaskTimerEvent.java
+++ TaskServer/src/main/java/top/dzurl/task/server/core/event/TaskTimerEvent.java
@@ -1,17 +1,16 @@
 package top.dzurl.task.server.core.event;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import top.dzurl.task.server.core.domain.Task;
 import top.dzurl.task.server.other.timer.event.SimpleTaskTimerEvent;
 
+@Slf4j
 @Component
 public class TaskTimerEvent implements SimpleTaskTimerEvent<Task> {
 
     @Override
     public void execute(Task task) {
-
-        System.out.println("time : " + task.getId());
-
-
+        log.info("time : " + task.getId());
     }
 }
